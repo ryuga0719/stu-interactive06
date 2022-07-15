b5bef131b31510dd4e75f9efab856ffd6a0520e0
ryuga0719 <ryuga.private@gmail.com>
2022-07-15 16:37:16 +0900

    create default text

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import { log } from "./modules/core/Debug";
+import gsap from "gsap";
+
+// ターゲットの移動量
+param = {
+  x: 0,
+  y: 0,
+};
+
+// マウス
+mouse = {
+  isDown: false, // 画面押されてるか
+  x: 0, // マウス位置 X
+  y: 0, // マウス位置 Y
+  start: { x: 0, y: 0 }, // マウスダウン時の座標
+  dist: { x: 0, y: 0 }, // マウスダウンしてからの移動距離
+};
